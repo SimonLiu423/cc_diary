3f7a87745cf389b48ff2c073692265a5ccc235d2
SimonLiu <simonliu423@gmail.com>
2024-07-02 00:32:06 +0800

    Feat: play music

--- lib/features/me/widgets/music_bar.dart
+++ lib/features/me/widgets/music_bar.dart
@@ -1,21 +1,117 @@
+import 'package:audio_session/audio_session.dart';
+import 'package:cc_diary/features/me/widgets/seek_bar.dart';
+import 'package:cc_diary/features/me/widgets/tmp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 
-class MusicBar extends StatelessWidget {
+class MusicBar extends StatefulWidget {
   const MusicBar({super.key});
+
+  @override
+  State<MusicBar> createState() => _MusicBarState();
+}
+
+class _MusicBarState extends State<MusicBar> with WidgetsBindingObserver {
+  final _player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    ambiguate(WidgetsBinding.instance)!.addObserver(this);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
+  }
+
+  Future<void> _init() async {
+    // Inform the operating system of our app's audio attributes etc.
+    // We pick a reasonable default for an app that plays speech.
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+      print('A stream error occurred: $e');
+    });
+    // Try to load audio from a source and catch any errors.
+    try {
+      // AAC example: https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.aac
+      await _player.setAudioSource(AudioSource.uri(Uri.parse(
+          "https://s3.amazonaws.com/scifri-episodes/scifri20181123-episode.mp3")));
+    } on PlayerException catch (e) {
+      print("Error loading audio source: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    ambiguate(WidgetsBinding.instance)!.removeObserver(this);
+    // Release decoders and buffers back to the operating system making them
+    // available for other apps to use.
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // Release the player's resources when not in use. We use "stop" so that
+      // if the app resumes later, it will still remember what position to
+      // resume from.
+      _player.stop();
+    }
+  }
+
+  /// Collects the data useful for displaying in a seek bar, using a handy
+  /// feature of rx_dart to combine the 3 streams of interest into one.
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          _player.positionStream,
+          _player.bufferedPositionStream,
+          _player.durationStream,
+          (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
-      child: const SizedBox(
-        width: double.infinity,
-        child: Row(
-          children: [
-            Icon(Icons.music_note),
-            SizedBox(width: 10),
-            Text('Music'),
-          ],
-        ),
+      onTap: () {
+        if (_player.playing) {
+          _player.pause();
+        } else {
+          _player.play();
+        }
+      },
+      child: Row(
+        children: [
+          const MusicTitle(),
+        ],
+      ),
+    );
+  }
+}
+
+class MusicTitle extends StatelessWidget {
+  const MusicTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // showDialog(
+        //   context: context,
+        //   builder: (context) => const Tmp(),
+        // );
+      },
+      child: Row(
+        children: [
+          Icon(Icons.music_note),
+          SizedBox(width: 10),
+          Text('Music'),
+        ],
       ),
     );
   }
