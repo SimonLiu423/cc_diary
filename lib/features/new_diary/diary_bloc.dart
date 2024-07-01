5c3d8bd51bba7d38d4b272798bc9f7f877fc86a3
WavJaby <WavJaby@gmail.com>
2024-07-02 01:23:15 +0800

    add diary save AI response popup

--- lib/features/new_diary/diary_bloc.dart
+++ lib/features/new_diary/diary_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'diary_event.dart';
@@ -13,6 +14,7 @@ class DiaryBloc extends Bloc<DiaryEvent, DiaryState> {
 
   FutureOr<void> _onSaveDiary(SaveDiary event, Emitter<DiaryState> emit) {
     log(event.diaryContent);
-    // emit(DiaryLoaded(event.songId, event.diaryContent, event.ccState));
+    emit(DiarySaving());
+    emit(DiarySaved(event.songId, "你嘻嘻了"));
   }
 }
