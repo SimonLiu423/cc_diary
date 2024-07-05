3855e59acb73761464e41848b228d29ea5189bf7
vac. <nckusic2022@gmail.com>
2024-07-06 05:40:47 +0800

    init new branch

--- lib/features/information/info.dart
+++ lib/features/information/info.dart
@@ -2,79 +2,43 @@ import 'package:cc_diary/features/new_diary/diary.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../l10n.dart';
+
 class InfoPage extends StatelessWidget {
   const InfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: SafeArea(
-      child: Row(children: [
-        Box(),
-        Box(),
-      ]),
-    ));
-  }
-}
-
-class Box extends StatelessWidget {
-  const Box({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      height: 500,
-      child: Column(
-        children: [
-          Image.network(
-            'https://picsum.photos/id/1003/200/300',
-            fit: BoxFit.cover,
-            width: 200,
-            height: 300,
-          ),
-          const Text('Box2'),
-          Counter(),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xffedede9),
+        appBar: AppBar(
+          title: Text(l10n(context).myDiaryPageTitle),
+        ),
+        body: GridView.count(
+          crossAxisCount: 3,
+          childAspectRatio: (1 / 1),
+          padding: const EdgeInsets.all(16.0),
+          mainAxisSpacing: 16.0,
+          crossAxisSpacing: 16.0,
+          children: List.generate(9, (index) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[850],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'images/depression.png',
+                  fit: BoxFit
+                      .cover, // This can be changed to fit your design needs
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
-}
 
-class Counter extends StatefulWidget {
-  const Counter({super.key});
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  late int _counter;
-
-  @override
-  void initState() {
-    super.initState();
-    _counter = 0;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(_counter.toString()),
-        TextButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DiaryPage(),
-                ),
-              );
-            },
-            child: Text(
-              '+',
-              style: TextStyle(fontSize: 30),
-            )),
-      ],
-    );
-  }
 }
